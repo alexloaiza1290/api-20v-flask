fb50940f4ece8f8a95b6c8acb6452fcd3ac879fe
jorgeav527 <jorgeav527@gmail.com>
2025-09-18 22:30:02 -0500

    casi create

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 
 def get_db_connection():
     connection = sqlite3.connect("database.db")
+    connection.row_factory = sqlite3.Row
     return connection
 
 app = Flask(__name__)
@@ -27,8 +28,19 @@ def get_all_post():
     c = get_db_connection()
     posts = c.execute('SELECT * FROM posts').fetchall()
     c.close()
-    return render_template("home.html", posts_list=posts)
+    return render_template("post/post_list.html", posts_list=posts)
 
+
+@app.route("/posts/create", methods=["GET", "POST"])
+def create_one():
+    if request.method == "GET":
+        return render_template("post/create.html")
+    if request.method == "POST":
+        title = request.form['title']
+        content = request.form['content']
+        print(title, content)
+        return redirect(url_for('get_all_post'))
+    
 
 if __name__ == '__main__': 
     app.run(debug=True)
