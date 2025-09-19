0e064bf55a83fadbab72f7ec93b06eab7eb99baf
jorgeav527 <jorgeav527@gmail.com>
2025-09-18 21:05:18 -0500

    adding about, home

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 from flask import Flask, render_template
+import sqlite3
+
+
+def get_db_connection():
+    connection = sqlite3.connect("database.db")
+    return connection
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
-def hola_mundo():
-    return "<p>Hello, mundo!</p>"
-
+def index():
+    name_page = "blog"
+    return render_template("index.html", name_page=name_page)
 
 @app.route("/home", methods=["GET"])
 def home():
-    numero_1 = "uno"
-    return render_template("index.html", numero_1=numero_1)
+    name_page = "home"
+    return render_template("home.html", name_page=name_page)
+
+@app.route("/about", methods=["GET"])
+def about():
+    name_page = "about"
+    return render_template("about.html", name_page=name_page)
 
 
 if __name__ == '__main__': 
