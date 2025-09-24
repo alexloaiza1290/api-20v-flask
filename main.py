8e46d58e3424cfdef43acbc82abdc83658b7863b
jorgeav527 <jorgeav527@gmail.com>
2025-09-23 21:06:38 -0500

    finalizando

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ def index():
     name_page = "blog"
     return render_template("index.html", name_page=name_page)
 
-@app.route("/home", methods=["GET"])
-def home():
-    name_page = "home"
-    return render_template("home.html", name_page=name_page)
+# @app.route("/home", methods=["GET"])
+# def home():
+#     name_page = "home"
+#     return render_template("post/post_list.html", name_page=name_page)
 
 @app.route("/about", methods=["GET"])
 def about():
@@ -30,17 +30,49 @@ def get_all_post():
     c.close()
     return render_template("post/post_list.html", posts_list=posts)
 
+@app.route('/posts/<int:post_id>', methods=['GET'])
+def get_one_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
+    conn.close()
+    return render_template('post/post.html', post=post)
 
 @app.route("/posts/create", methods=["GET", "POST"])
-def create_one():
+def create_one_post():
     if request.method == "GET":
         return render_template("post/create.html")
     if request.method == "POST":
         title = request.form['title']
         content = request.form['content']
-        print(title, content)
+        conn = get_db_connection()
+        conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)', (title, content))
+        conn.commit()
+        conn.close()
         return redirect(url_for('get_all_post'))
-    
+
+@app.route('/posts/edit/<int:post_id>', methods=['GET', 'POST'])
+def edit_one_post(post_id):
+    if request.method == "GET":
+        conn = get_db_connection()
+        post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
+        conn.close()
+        return render_template('post/update.html', post=post)
+    if request.method == "POST":
+        conn = get_db_connection()
+        title = request.form['title']
+        content = request.form['content']
+        conn.execute('UPDATE posts SET title = ?, content = ? WHERE id = ?', (title, content, post_id))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('get_all_post'))
+
+@app.route('/posts/delete/<int:post_id>', methods=['DELETE'])
+def delete_one_post(post_id):
+    conn = get_db_connection()
+    conn.execute('DELETE FROM posts WHERE id = ?', (post_id,))
+    conn.commit()
+    conn.close()
+    return ""
 
 if __name__ == '__main__': 
     app.run(debug=True)
